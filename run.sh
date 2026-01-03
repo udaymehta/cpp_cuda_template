9be2bc75857e2e85af93c5142b33a87a940ab5d4
udaymehta <udaymehta.1011@gmail.com>
2026-01-03 13:04:37 +0530

    chore: Ninja as default build configuration

--- run.sh
+++ run.sh
@@ -15,10 +15,17 @@ else
     exit 1
 fi
 
-if [ ! -f "$BUILD_DIR/Makefile" ] && [ ! -f "$BUILD_DIR/build.ninja" ]; then
-    echo "Configuring $CMAKE_TYPE build..."
-    cmake -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE="$CMAKE_TYPE"
+# Ninja (speed voooooooo!)
+if [ ! -f "$BUILD_DIR/build.ninja" ]; then
+    echo "Configuring $CMAKE_TYPE build with Ninja..."
+    cmake -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE="$CMAKE_TYPE" -G Ninja
 fi
+
+# Makefile (default)
+# if [ ! -f "$BUILD_DIR/Makefile" ] && [ ! -f "$BUILD_DIR/build.ninja" ]; then
+#     echo "Configuring $CMAKE_TYPE build..."
+#     cmake -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE="$CMAKE_TYPE"
+# fi
 
 echo "Building ($CMAKE_TYPE)..."
 cmake --build "$BUILD_DIR" -j$(nproc)
